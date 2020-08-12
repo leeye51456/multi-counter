cf7a2b32b21485e3cc78833978401c3966c84aca
leeye51456 <leeye51456@gmail.com>
2020-08-12 20:47:19 +0900

    Make shortcut form to controlled component

--- src/shortcut-capture-form.jsx
+++ src/shortcut-capture-form.jsx
@@ -1,35 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import utils from './utils';
 
 class ShortcutCaptureForm extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      key: '',
-      code: '',
-      shiftKey: false,
-    };
-  }
-
   handleKeyDown = (event) => {
     const { key, shiftKey } = event;
     const { code } = event.nativeEvent;
 
-    if (!code) {
-      if (utils.isValidKey(key)) {
-        this.setState({ key, shiftKey });
+    if (code) {
+      if (utils.isValidCode(code)) {
+        this.props.onChange({ code, shiftKey });
+      } else {
+        this.props.onChange({ code: '', shiftKey: false });
       }
-      return;
-    }
 
-    if (utils.isValidCode(code)) {
-      this.setState({ code, shiftKey });
+    } else {
+      if (utils.isValidKey(key)) {
+        this.props.onChange({ keyName: key, shiftKey });
+      } else {
+        this.props.onChange({ keyName: '', shiftKey: false });
+      }
     }
   }
 
   getShortcutString = () => {
-    const { key, code, shiftKey } = this.state;
+    const { keyName, code, shiftKey } = this.props;
     let shift;
     if (utils.isMacOs) {
       shift = shiftKey ? '⇧' : '';
@@ -40,24 +35,34 @@ class ShortcutCaptureForm extends React.Component {
     if (code) {
       return `${shift}${utils.getNotShiftedFromCode(code)}`;
     }
-    return `${shift}${utils.getNotShiftedFromKey(key)}`;
+    return `${shift}${utils.getNotShiftedFromKey(keyName)}`;
   }
 
   render = () => {
     return (
-      <ul>
-        <li>
-          <input
-            type="text"
-            value={this.getShortcutString()}
-            onChange={utils.no_op}
-            onKeyDown={this.handleKeyDown}
-            style={{ textAlign: 'right' }}
-          />
-        </li>
-      </ul>
+      <input
+        type="text"
+        value={this.getShortcutString()}
+        onChange={utils.no_op}
+        onKeyDown={this.handleKeyDown}
+        // style={{ textAlign: 'right' }}
+      />
     );
   }
 }
+
+ShortcutCaptureForm.propTypes = {
+  keyName: PropTypes.string,
+  code: PropTypes.string,
+  shiftKey: PropTypes.bool,
+  onChange: PropTypes.func,
+}
+
+ShortcutCaptureForm.defaultProps = {
+  keyName: '',
+  code: '',
+  shiftKey: false,
+  onChange: utils.no_op,
+};
 
 export default ShortcutCaptureForm;
