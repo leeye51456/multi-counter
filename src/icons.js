5fdb46ea23552221aa13249aa82bab3f71ad1735
leeye51456 <leeye51456@gmail.com>
2020-09-09 01:44:33 +0900

    Add icons

---
+++ src/icons.js
@@ -0,0 +1,13 @@
+import add from './icons/add.svg';
+import check from './icons/check.svg';
+import checkAll from './icons/check-all.svg';
+import clear from './icons/clear.svg';
+import close from './icons/close.svg';
+import edit from './icons/edit.svg';
+import menu from './icons/menu.svg';
+import remove from './icons/remove.svg';
+import reset from './icons/reset.svg';
+import subtract from './icons/subtract.svg';
+
+const icons = { add, check, checkAll, clear, close, edit, menu, remove, reset, subtract };
+export default icons;
