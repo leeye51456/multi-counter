a04a8c97dd344b4ab60f3409e08e3a7b3386f4d8
leeye51456 <leeye51456@gmail.com>
2020-09-24 17:08:47 +0900

    Add Korean translation

--- src/i18n.js
+++ src/i18n.js
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import translationEn from './locales/en.json';
+import translationKo from './locales/ko.json';
 
 const languageDetectorOptions = {
   order: ['querystring', 'navigator'],
@@ -11,6 +12,9 @@ const languageDetectorOptions = {
 const resources = {
   en: {
     translation: translationEn,
+  },
+  ko: {
+    translation: translationKo,
   },
 };
 
