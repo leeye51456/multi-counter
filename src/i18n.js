3a4838ce449d474075c401e8376d8d748ffb57c2
leeye51456 <leeye51456@gmail.com>
2020-09-24 16:42:32 +0900

    Apply LanguageDetector to i18n

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,12 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import translationEn from './locales/en.json';
+
+const languageDetectorOptions = {
+  order: ['querystring', 'navigator'],
+  lookupQuerystring: 'lang',
+};
 
 const resources = {
   en: {
@@ -9,10 +15,13 @@ const resources = {
 };
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources,
-    lng: 'en',
+
+    fallbackLng: 'en',
+    detection: languageDetectorOptions,
 
     interpolation: {
       escapeValue: false,
