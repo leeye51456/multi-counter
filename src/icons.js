2eb4aaa335d04a22fba34e1f20e31f4f735f0780
leeye51456 <leeye51456@gmail.com>
2020-09-29 01:10:34 +0900

    Add language icon

--- src/icons.js
+++ src/icons.js
@@ -5,10 +5,11 @@ import clear from './icons/clear.svg';
 import close from './icons/close.svg';
 import edit from './icons/edit.svg';
 import invert from './icons/invert.svg';
+import language from './icons/language.svg';
 import menu from './icons/menu.svg';
 import remove from './icons/remove.svg';
 import reset from './icons/reset.svg';
 import subtract from './icons/subtract.svg';
 
-const icons = { add, check, checkAll, clear, close, edit, invert, menu, remove, reset, subtract };
+const icons = { add, check, checkAll, clear, close, edit, invert, language, menu, remove, reset, subtract };
 export default icons;
